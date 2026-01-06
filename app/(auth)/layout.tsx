8a44679063e984aa3e70b5ccd659eeefc623ee35
highsky <highsky33570@gmail.com>
2026-01-06 10:13:26 +0800

    clear

--- app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type React from "react"
-
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <div className="h-screen w-full overflow-hidden bg-content1">{children}</div>
-}
